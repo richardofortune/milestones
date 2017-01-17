96484f98cc4329cc65f817ed0a324cb923f068cf
Ben Biddington <ben.biddington@xero.com>
2017-01-18 12:40:14 +1300

    Delete milestones first

--- install.sh
+++ install.sh
@@ -6,6 +6,26 @@ username=$1
 repo="onboarding_me"
 accessToken=$2
 
+function post {
+  path=$1
+  body=$2
+  url=https://api.github.com/repos/$username/$repo$path
+
+  # echo "url: $url"
+
+  curl -u $username:$accessToken -iL -X POST -d "$body" -H 'Content-Type: application/json' "$url"
+}
+
+function _delete {
+  path=$1
+
+  url=https://api.github.com/repos/$username/$repo$path
+
+  # echo "url: $url"
+
+  curl -u $username:$accessToken -X DELETE "$url"
+}
+
 function addLabels {
   echo "Creating labels"
 
@@ -14,18 +34,30 @@ function addLabels {
   curl -u $username:$accessToken -iL -X POST -d '{ "name": "2 Hard", "color" : "ff0000" }' -H 'Content-Type: application/json' "https://api.github.com/repos/$username/$repo/labels"
 }
 
-echo "Switching on issues for username <$username> using access token <$accessToken> using <https://api.github.com/repos/$username/$repo>"
+# todo: add emoji
+function addMilestones {
+  echo "Creating milestones"
 
-json='{ "name": "'$repo'", "has_issues": "true" }'
+  _delete '/milestones/1'
+  _delete '/milestones/2'
+  _delete '/milestones/3'
 
-curl -u $username:$accessToken -iL -X PATCH -d "$json" -H 'Content-Type: application/json' "https://api.github.com/repos/$username/$repo"
-curl -u $username:$accessToken -iL -X PATCH -d "$json" -H 'Content-Type: application/json' "https://api.github.com/repos/$username/$repo"
-curl -u $username:$accessToken -iL -X PATCH -d "$json" -H 'Content-Type: application/json' "https://api.github.com/repos/$username/$repo"
+  post '/milestones' '{ "title": "00 The People and The tools" }' -H 'Content-Type: application/json'
+  post '/milestones' '{ "title": "01 Getting hands on" }' -H 'Content-Type: application/json'
+  post '/milestones' '{ "title": "02 Doing your best work" }' -H 'Content-Type: application/json'
+}
 
-echo "Creating an issue for username <$username> using access token <$accessToken> using <https://api.github.com/repos/$username/$repo/issues>"
+function addIssues {
+  echo "Switching on issues for username <$username> using access token <$accessToken> using <https://api.github.com/repos/$username/$repo>"
 
-json='{ "title": "example" }'
+  json='{ "name": "'$repo'", "has_issues": "true" }'
 
-curl -u $username:$accessToken -iL -X POST -d "$json" -H 'Content-Type: application/json' "https://api.github.com/repos/$username/$repo/issues"
+  echo "Creating an issue for username <$username> using access token <$accessToken> using <https://api.github.com/repos/$username/$repo/issues>"
 
-addLabels
+  json='{ "title": "example" }'
+
+  curl -u $username:$accessToken -iL -X POST -d "$json" -H 'Content-Type: application/json' "https://api.github.com/repos/$username/$repo/issues"
+}
+
+#addLabels
+addMilestones
