125cf2340185571229313ff976c80e1791b988ea
Ben Biddington <ben.biddington@xero.com>
2017-01-18 12:13:49 +1300

    Can create labels (fails if they exist already)

--- install.sh
+++ install.sh
@@ -6,10 +6,20 @@ username=$1
 repo="onboarding_me"
 accessToken=$2
 
+function addLabels {
+  echo "Creating labels"
+
+  curl -u $username:$accessToken -iL -X POST -d '{ "name": "1 Easy", "color" : "00ff00" }' -H 'Content-Type: application/json' "https://api.github.com/repos/$username/$repo/labels"
+  curl -u $username:$accessToken -iL -X POST -d '{ "name": "1 Medium" }' -H 'Content-Type: application/json' "https://api.github.com/repos/$username/$repo/labels"
+  curl -u $username:$accessToken -iL -X POST -d '{ "name": "2 Hard", "color" : "ff0000" }' -H 'Content-Type: application/json' "https://api.github.com/repos/$username/$repo/labels"
+}
+
 echo "Switching on issues for username <$username> using access token <$accessToken> using <https://api.github.com/repos/$username/$repo>"
 
 json='{ "name": "'$repo'", "has_issues": "true" }'
 
+curl -u $username:$accessToken -iL -X PATCH -d "$json" -H 'Content-Type: application/json' "https://api.github.com/repos/$username/$repo"
+curl -u $username:$accessToken -iL -X PATCH -d "$json" -H 'Content-Type: application/json' "https://api.github.com/repos/$username/$repo"
 curl -u $username:$accessToken -iL -X PATCH -d "$json" -H 'Content-Type: application/json' "https://api.github.com/repos/$username/$repo"
 
 echo "Creating an issue for username <$username> using access token <$accessToken> using <https://api.github.com/repos/$username/$repo/issues>"
@@ -17,3 +27,5 @@ echo "Creating an issue for username <$username> using access token <$accessToke
 json='{ "title": "example" }'
 
 curl -u $username:$accessToken -iL -X POST -d "$json" -H 'Content-Type: application/json' "https://api.github.com/repos/$username/$repo/issues"
+
+addLabels
