c05fd0bdca2974855f834507388edd23251f1b48
Ben Biddington <ben.biddington@xero.com>
2017-01-18 12:03:52 +1300

    Can create issues

--- install.sh
+++ install.sh
@@ -3,9 +3,17 @@
 # usage: install.sh ben-biddington repo-name {api key}
 
 username=$1
-repo=onboarding_me
+repo="onboarding_me"
 accessToken=$2
 
 echo "Switching on issues for username <$username> using access token <$accessToken> using <https://api.github.com/repos/$username/$repo>"
 
-curl -u $username:$accessToken -iL -X PATCH -d '{ "name": "$repo", "has_issues":"true"}' -H 'Content-Type: application/json' "https://api.github.com/repos/$username/$repo"
+json='{ "name": "'$repo'", "has_issues": "true" }'
+
+curl -u $username:$accessToken -iL -X PATCH -d "$json" -H 'Content-Type: application/json' "https://api.github.com/repos/$username/$repo"
+
+echo "Creating an issue for username <$username> using access token <$accessToken> using <https://api.github.com/repos/$username/$repo/issues>"
+
+json='{ "title": "example" }'
+
+curl -u $username:$accessToken -iL -X POST -d "$json" -H 'Content-Type: application/json' "https://api.github.com/repos/$username/$repo/issues"
