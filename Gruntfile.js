392163c39b8e26d8ef7e51139c046a9c5bd7e6a9
Richard Fortune <richard.fortune@gmail.com>
2017-01-18 17:27:03 +1300

    updated port(8080)

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,7 +68,7 @@ module.exports = function(grunt) {
     connect: {
       src: {
         options: {
-          port: 8080
+          port: 8080,
           hostname: '*',
           base: 'src/',
           open: true,
